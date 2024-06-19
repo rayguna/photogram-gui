8f0bceadf75d705bdf7855deebf8032c38e8503f
Ray Gunawidjaja <33289654+rayguna@users.noreply.github.com>
2024-06-19 17:23:37 +0000

    Removed irrelevant html.erb files and edited comment.rb for improved clarity.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -11,7 +11,8 @@
 #
 
 class Comment < ApplicationRecord
-  validates(:commenter, { :presence => true })
+  #validates(:commenter, { :presence => true })
+  validates(:author_id, { :presence => true }) #validation using author_id is more intuitive as this field is a part of the comments table.
 
   def commenter
     my_author_id = self.author_id
